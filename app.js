968df8a170b37154466acd35807385df7b8bc930
= <jacex17@gmail.com>
2020-03-08 22:43:58 -0400

    first commit to deploy

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.use(cors({
+  origin: ["http://localhost:3000"]
+}))
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
---
+++ public/precache-manifest.aa0b009cbea8137dd80b7ddf27ed631b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac77c052c65dbbccc388bd0af73747f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "694231c6a05a13c99f01",
+    "url": "/static/css/main.01bfe1c0.chunk.css"
+  },
+  {
+    "revision": "dcddef90620ccb0f8441",
+    "url": "/static/js/2.e0fa90fd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e0fa90fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "694231c6a05a13c99f01",
+    "url": "/static/js/main.eee2f96e.chunk.js"
+  },
+  {
+    "revision": "77f42cd08f6dab2d353a",
+    "url": "/static/js/runtime-main.4c15ebf4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
